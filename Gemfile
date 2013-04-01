1d168e289c59789e0956876ecdbe85d12b2c0b83
Dan Allen <dallen@redhat.com>
2013-04-01 03:39:31 -0600

    initial import

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'compass', '0.12.2'
+gem 'zurb-foundation', '4.0.9'
---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'zurb-foundation'
+require './lib/functions'
+
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'javascripts'
+
+# :expanded, :nested, :compact or :compressed
+output_style = :compact
+#output_style = :compressed
+
+relative_assets = true
+
+#line_comments = true
+line_comments = false
+
+# doesn't seem to work
+#asset_cache_buster = false
