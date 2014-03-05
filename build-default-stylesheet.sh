d27d04b6ca5bad62329c769b76aa75bde5882830
Dan Allen <dan.j.allen@gmail.com>
2014-03-05 03:06:08 -0700

    remove font-awesome import, make script more readable

--- build-default-stylesheet.sh
+++ build-default-stylesheet.sh
@@ -4,4 +4,9 @@ compass compile -s compact
 LINES=`wc -l stylesheets/asciidoctor.css | cut -d" " -f1`
 let LINES=$LINES-1
 echo '/* Asciidoctor default stylesheet | MIT License | http://asciidoctor.org */' > asciidoctor.css
-cat stylesheets/asciidoctor.css | tail --lines=$LINES | sed 's/ *\/\*\+!\? [^*]\+\($\| \*\/\)//g' | sed 's/^\/\*\* .* \*\/$//' | sed '/^\(*\/\|\) *$/d' >> asciidoctor.css
+cat stylesheets/asciidoctor.css | \
+  tail --lines=$LINES | \
+  sed 's/ *\/\*\+!\? [^*]\+\($\| \*\/\)//g' | \
+  sed 's/^\/\*\* .* \*\/$//' | \
+  sed '/^\(*\/\|\) *$/d' | \
+  grep -v 'font-awesome' >> asciidoctor.css
