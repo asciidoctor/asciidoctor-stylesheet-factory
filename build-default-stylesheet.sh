043274aedb6b9f35ee976c83146f0444bed1ded4
Dan Allen <dan.j.allen@gmail.com>
2014-09-19 02:27:37 -0600

    allow stylesheet name to be specified in build script

--- build-default-stylesheet.sh
+++ build-default-stylesheet.sh
@@ -1,23 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+STYLESHEET_NAME=asciidoctor
+
+if [ ! -z $1 ]; then
+  STYLESHEET_NAME=$1
+fi
 
 compass compile -s compact
-LINES=`wc -l stylesheets/asciidoctor.css | cut -d" " -f1`
-echo '/* Asciidoctor default stylesheet | MIT License | http://asciidoctor.org */' > asciidoctor.css
-cat stylesheets/asciidoctor.css | \
+LINES=`wc -l stylesheets/$STYLESHEET_NAME.css | cut -d" " -f1`
+echo '/* Asciidoctor default stylesheet | MIT License | http://asciidoctor.org */' > $STYLESHEET_NAME.css
+cat stylesheets/$STYLESHEET_NAME.css | \
   sed 's/ *\/\*\+!\? [^*]\+\($\| \*\/\)//g' | \
   sed 's/^\/\*\* .* \*\/$//' | \
   sed '/^\(*\/\|\) *$/d' | \
   sed '/\(meta\.\|\.vcard\|\.vevent\)/d' | \
-  grep -v 'font-awesome' >> asciidoctor.css
+  grep -v 'font-awesome' >> $STYLESHEET_NAME.css
 
 # see https://www.npmjs.org/package/cssshrink
-cssshrink asciidoctor.css | \
+cssshrink $STYLESHEET_NAME.css | \
   sed '1i\
 /* Remove the comments around the @import statement below when using this as a custom stylesheet */\
 /*@import "https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic|Noto+Serif:400,400italic,700,700italic|Droid+Sans+Mono:400";*/' | \
   sed '1i\
 /* Asciidoctor default stylesheet | MIT License | http://asciidoctor.org */' | \
-  sed 's/\(Open Sans\|DejaVu Sans\|Noto Serif\|DejaVu Serif\|Droid Sans Mono\|DejaVu Sans Mono\)/"\1"/g' | \
+  sed 's/\(Open Sans\|DejaVu Sans\|Noto Serif\|DejaVu Serif\|Droid Sans Mono\|DejaVu Sans Mono\|Ubuntu Mono\|Liberation Mono\|Varela Round\)/"\1"/g' | \
   sed 's/font-weight:700/font-weight:bold/g' | \
-  sed 's/\([^}]\)}\([^}]\)/\1}\n\2/g' > asciidoctor.min.css
-#  sed 's/!important/ &/g' | \
+  sed 's/\([^}]\)}\([^}]\)/\1}\n\2/g' > $STYLESHEET_NAME.min.css
