177c383e47aaf7b76cd688205452acc5974e44b1
Dan Allen <dan.j.allen@gmail.com>
2015-10-30 01:38:16 -0600

    trim stylesheet more; fix missing endlines in final version

--- build-stylesheet.sh
+++ build-stylesheet.sh
@@ -13,16 +13,19 @@ cat stylesheets/$STYLESHEET_NAME.css | \
   sed 's/ *\/\*\+!\? [^*]\+\($\| \*\/\)//g' | \
   sed 's/^\/\*\* .* \*\/$//' | \
   sed '/^\(*\/\|\) *$/d' | \
+  sed 's/\.antialiased, body/body/' | \
+  sed '/object, svg { display: inline-block;/d' | \
+  sed 's/img { display: inline-block;/img, object, svg { display: inline-block;/' | \
   sed '/\(meta\.\|\.vcard\|\.vevent\|#map_canvas\)/d' | \
   grep -v 'font-awesome' >> $STYLESHEET_NAME.css
 
 # see https://www.npmjs.org/package/cssshrink
 cssshrink $STYLESHEET_NAME.css | \
   sed '1i\
-/* Remove comment around the @import statement below when using this as a custom stylesheet */\
+/* Remove comment around @import statement below when using as a custom stylesheet */\
 /*@import "https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic%7CNoto+Serif:400,400italic,700,700italic%7CDroid+Sans+Mono:400,700";*/' | \
   sed '1i\
 /* Asciidoctor default stylesheet | MIT License | http://asciidoctor.org */' | \
   sed 's/\(Open Sans\|DejaVu Sans\|Noto Serif\|DejaVu Serif\|Droid Sans Mono\|DejaVu Sans Mono\|Ubuntu Mono\|Liberation Mono\|Varela Round\)/"\1"/g' | \
   sed 's/font-weight:700/font-weight:bold/g' | \
-  sed 's/\([^}]\)}\([^}]\)/\1}\n\2/g' > $STYLESHEET_NAME.min.css
+  ruby -e 'puts STDIN.read.gsub(/}(?!})/, %(}\n)).chomp' - > $STYLESHEET_NAME.min.css
